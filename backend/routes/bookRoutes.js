95048fb0ec536fe3b98cdb7871834884e423a491
Rafael <rafaelmoura7033@gmail.com>
2025-06-22 13:06:24 -0300

    feat: reorder routes in bookRoutes for better organization and clarity

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -2,17 +2,21 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
 
-router.post('/', bookController.createBook);
-router.get('/', bookController.getAllBooks);
-router.get('/:id', bookController.getById);
+// Consultas específicas
 router.get('/autor/:autor', bookController.getByAuthor);
 router.get('/genero/:genero', bookController.getByGenre);
 router.get('/ano/:ano', bookController.getByYear);
+router.get('/isbn/:isbn', bookController.getByISBN);
 router.get('/mais-paginas', bookController.getTopPages);
 router.get('/menos-paginas', bookController.getLeastPages);
-router.get('/isbn/:isbn', bookController.getByISBN);
+
+// CRUD
+router.get('/', bookController.getAllBooks);
+router.post('/', bookController.createBook);
 router.put('/:id', bookController.updateBook);
 router.delete('/:id', bookController.deleteBook);
 
+// IMPORTANTE: essa deve ser a ÚLTIMA rota
+router.get('/:id', bookController.getById);
 
 module.exports = router;
