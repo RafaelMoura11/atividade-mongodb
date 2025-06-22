515cf6bb59388820d8689126a5d3a06ba8918bc4
Rafael <rafaelmoura7033@gmail.com>
2025-06-22 12:50:31 -0300

    feat: add route for fetching a book by ID in bookRoutes

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -4,6 +4,7 @@ const bookController = require('../controllers/bookController');
 
 router.post('/', bookController.createBook);
 router.get('/', bookController.getAllBooks);
+router.get('/:id', bookController.getById);
 router.get('/autor/:autor', bookController.getByAuthor);
 router.get('/genero/:genero', bookController.getByGenre);
 router.get('/ano/:ano', bookController.getByYear);
