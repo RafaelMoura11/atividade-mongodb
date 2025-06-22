288f6e6c691f46d316707745b4aa73f8adee331a
Rafael <rafaelmoura7033@gmail.com>
2025-06-22 11:55:50 -0300

    feat: implement createBook function in bookController

---
+++ backend/controllers/bookController.js
@@ -0,0 +1,11 @@
+const Book = require('../models/Book');
+
+exports.createBook = async (req, res) => {
+  try {
+    const newBook = new Book(req.body);
+    await newBook.save();
+    res.status(201).json(newBook);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
